2c34b6a08de7fc2b2e40f659ac94354733448fd6
Sam Collier <samcollier916@gmail.com>
2020-05-14 23:05:05 +0000

    Added JCudaTest

--- Ray Tracer/src/JCudaTest.java
+++ Ray Tracer/src/JCudaTest.java
@@ -5,9 +5,10 @@ public class JCudaTest
 {
     public static void main(String args[])
     {
+    	// Still learning, but I'm hoping to get this project rendered through the GPU
         Pointer pointer = new Pointer();
         JCuda.cudaMalloc(pointer, 4);
         System.out.println("Pointer: "+pointer);
-        JCuda.cudaFree(pointer);
+        JCuda.cudaFree(pointer);;
     }
 }
